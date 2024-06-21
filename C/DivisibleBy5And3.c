840d2e5fa2c5ef6ae31d79ab6817ae3d1335f208
Trigungt <triguntamragouri@gmail.com>
2024-06-21 10:19:56 +0530

    DivisibleBy5And3

---
+++ C/DivisibleBy5And3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num%5==0 && num%3==0)
+    {
+        printf("%d is divisible by 5 and 3",num);
+    }
+    else if(num%5==0 || num%3==0)
+    {
+        printf("%d is divisible by 5 or 3",num);
+    }
+    else
+    {
+        printf("%d is not divisible by 5 and 3",num);
+    }
+    return 0;
+}
