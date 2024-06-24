ababe7910826de029773dca9afdf896f2b069b9b
Trigungt <triguntamragouri@gmail.com>
2024-06-24 15:42:31 +0530

    StarTriangleUlta

--- C/StarTriangleUlta.c
+++ C/StarTriangleUlta.c
@@ -4,12 +4,14 @@ int main()
     int m;
     printf("Enter no of rows: ");
     scanf("%d",&m);
+    int a=m;
     for(int i=1;i<=m;i++)
     {
-        for(int j=1;j<=m+1-i;j++)
+        for(int j=1;j<=a;j++)
         {
             printf("* ");
         }
+        a--;
         printf("\n");
     }
     return 0;
