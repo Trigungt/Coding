61eea428b62188f67092fdea5083c84b5f109741
Trigungt <triguntamragouri@gmail.com>
2024-06-24 11:24:40 +0530

    SolidSquare

---
+++ C/SolidSquare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("Enter no of rows and columns: ");
+    scanf("%d",&m);
+    for(int i=1;i<=m;i++)     
+    {
+        for(int j=1;j<=m;j++)  
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
