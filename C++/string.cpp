90913d920eb8c27f219c41c2dc981e656d2caeb0
Trigungt <triguntamragouri@gmail.com>
2024-06-25 19:07:51 +0530

    string

---
+++ C++/string.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h> // includes all libraries
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin,str);
+    cout<<"String entered is: "<<str;
+    return 0;
+}
