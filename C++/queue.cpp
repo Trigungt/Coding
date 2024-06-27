dcbe95b927a2c0ff6f144f54a801889e13c2f119
Trigungt <triguntamragouri@gmail.com>
2024-06-27 18:36:42 +0530

    queue

---
+++ C++/queue.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    queue<int>q;
+    q.push(1); // 1
+    q.push(2); // 1 2
+    q.push(3); // 1 2 3
+    q.push(4); // 1 2 3 4
+    q.back()+=6;
+    cout<<q.back()<<endl; // 10
+    cout<<q.front()<<endl; // 1
+    q.pop();
+    cout<<q.front();
+    return 0;
+}
