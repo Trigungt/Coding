693c150294e9f7d526701865577bf207ff410b06
Trigungt <triguntamragouri@gmail.com>
2024-06-27 16:31:59 +0530

    stack

---
+++ C++/stack.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int>s;
+    s.push(1); // 1
+    s.push(2); // 2 1
+    s.push(3); // 3 2 1
+    s.push(4); // 4 3 2 1
+
+    cout<<s.top()<<endl; // 4
+    s.pop(); // s looks like 3 2 1
+    cout<<s.top()<<endl; // 3
+    cout<<s.size()<<endl;
+    cout<<s.empty();
+    return 0;
+}
