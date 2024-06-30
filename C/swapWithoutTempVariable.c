7bbb0c6f95d4e387fd11cf58c9ee03ae89742f7f
Trigungt <triguntamragouri@gmail.com>
2024-06-30 16:37:44 +0530

    swapWithoutTempVariable

---
+++ C/swapWithoutTempVariable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+    num1=num1+num2;
+    num2=num1-num2;
+    num1=num1-num2;
+    printf("First number after swapping is: %d\n",num1);
+    printf("Second number after swapping is: %d",num2);
+    return 0;
+}
