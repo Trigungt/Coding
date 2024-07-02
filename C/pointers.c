538c706e3e0ea59e25275b71128fab07bf87f65c
Trigungt <triguntamragouri@gmail.com>
2024-07-02 23:37:18 +0530

    pointers

--- C/pointers.c
+++ C/pointers.c
@@ -2,6 +2,7 @@
 int main()
 {
     int num=56;
-    printf("%p",&num); // %p --> prints the address.Here, prints the address of num variable 
+    int* x=&num;
+    printf("%p",x); // %p --> prints the address.Here, prints the address of num variable 
     return 0;
 }
